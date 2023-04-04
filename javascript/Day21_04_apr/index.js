9e3073ecba69d6c5089c7c96ee8fb56f0d27db94
Utkarsh Gupta <utkarsh.gupta1@housing.com>
2023-04-04 21:59:41 +0530

    Day21

---
+++ javascript/Day21_04_apr/index.js
@@ -0,0 +1,103 @@
+var x = 4;
+var y = 2;
+var z = x+y; 
+
+var karishmaAge = 4;
+var rameshAge = 2;
+
+
+var totalAge = karishmaAge % rameshAge;
+
+console.log(totalAge,z)
+
+
+var number = 100004;
+
+var remainder = number % 2;
+
+
+console.log(remainder)
+
+
+
+var num; //declaration
+
+
+num = 4; // initialization
+
+console.log(num)
+
+
+num = 5; // assignment
+
+console.log(num)
+
+
+var rameshFamilyName = 2; // camel Case;
+
+var TotalSum = 4; // Pascal Case;
+
+var apple_product_price = 50; // snake case
+
+
+var MAX_SIZE = 3000;
+var Z_INDEX = 999999;
+
+//premitive data type
+var name = "Utkarsh"
+var age = 24
+var isElegible = true;
+var bloodGroup = 'A';
+var weight = 62.5;
+
+
+var snehaAge = 22;
+
+var nagmaAge = 21;
+
+console.log(snehaAge>nagmaAge);
+
+var apples = 32;
+
+var oranges = 27;
+
+console.log(apples<oranges);
+
+
+var heightA = 6;
+
+var heightB = 6;
+
+console.log(heightA>=heightB)
+console.log(heightA<=heightB)
+
+
+var sateshFatherName = "Sateesh"
+
+var jainendraBrotherName = "Rahul";
+
+console.log(jainendraBrotherName==sateshFatherName)
+
+
+var swtichOn = 1;
+var switchOff = 0;
+
+var lightsOn = '1'
+var lightsOff = '0'
+
+
+
+console.log(swtichOn===lightsOn);
+
+console.log(swtichOn!=switchOff);
+
+console.log(switchOff!==lightsOff);
+
+
+
+
+
+
+
+
+
