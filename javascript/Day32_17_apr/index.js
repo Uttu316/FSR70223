c7a92100f3aaea5062e9cf85d659f68c8d40f705
Utkarsh Gupta <utkarsh.gupta1@housing.com>
2023-04-17 22:41:16 +0530

    Day 32

---
+++ javascript/Day32_17_apr/index.js
@@ -0,0 +1,27 @@
+let arr = [1,2,3,4,5]
+
+arr.push(-1)
+
+console.log(arr)
+
+
+arr.pop()
+
+console.log(arr)
+
+
+arr.unshift(-1); // adds value in start
+
+console.log(arr)
+
+arr.shift(); // removes value from start
+
+console.log(arr)
+
+
+
+let arrr = new Array(5).fill(-1);
+
+const ele = arr.find((i)=>i==3);
+
+console.log(ele)
