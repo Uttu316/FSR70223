324653ea9d98384ef0259cb5a8390103485c6126
Utkarsh Gupta <utkarsh.gupta1@housing.com>
2023-04-25 21:57:12 +0530

    Day 39

--- javascript/Day39_25_apr/index.js
+++ javascript/Day39_25_apr/index.js
@@ -20,3 +20,43 @@ const obj = {
   
   
   
+
+  const ul = document.createElement('ul');
+
+ul.classList.add('list');
+
+
+const names = ['Ajay','Vijay','Simran',"Sneha"];
+
+names.forEach((i)=>{
+    
+  const li = document.createElement('li');
+   
+  li.innerHTML = i;
+  
+  ul.append(li)
+})
+
+const container = document.querySelector('#container')
+
+container.append(ul);
+
+
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click',function(e){
+  console.log(e)
+  const li = document.createElement('li');
+   
+  li.innerHTML = "Happy";
+  
+  const container = document.querySelector('#container ul')
+  
+  container.append(li)
+
+})
+
+
+window.addEventListener('load',function(e){
+    console.log('Doument Ready')
+})
