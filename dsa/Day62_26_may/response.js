9c53ac64a04fe981fcedc855cab7d75b54cb0acc
Utkarsh Gupta <utkarsh.gupta1@housing.com>
2023-05-27 04:23:05 +0530

    Day 62

--- dsa/Day62_26_may/response.js
+++ dsa/Day62_26_may/response.js
@@ -1,1 +1,12 @@
-{ head: Node { data: 4, next: null } }
+{
+    "head": {
+        "data": 2,
+        "next": {
+            "data": 3,
+            "next": {
+                "data": 4,
+                "next": null
+            }
+        }
+    }
+}
